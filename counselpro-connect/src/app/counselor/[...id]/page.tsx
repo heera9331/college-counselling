17bc3d73b5b3029b5be96589f0bd66745188a659
heera9331 <heera9331@gmail.com>
2024-03-18 15:32:41 +0530

    counselor done

--- counselpro-connect/src/app/counselor/[...id]/page.tsx
+++ counselpro-connect/src/app/counselor/[...id]/page.tsx
@@ -1,6 +1,8 @@
+"use client"
 import axios from 'axios';
 import { apiBaseUrl } from "@/utils";
 import { Students } from "@/components"
+import { useEffect, useState } from 'react';
 
 const getUser = async (id: string) => {
     let res = await axios.get(`${apiBaseUrl}/api/users/${id}`);
@@ -23,20 +25,41 @@ const getStudents = async (registeredBy: string) => {
     return [];
 }
 
-const Page = async ({ params }: { params: object }) => {
-
+const Page = ({ params }: { params: object }) => {
+    const [students, setStudents] = useState([])
+    const [user, setUser] = useState<{ name: string, email: string, isAdmin: boolean } | null>(null);
     const id = params.id[0];
-    const user = await getUser(id);
-    const students = await getStudents(id);
 
-    console.log('user', user);
+    useEffect(() => {
+        ; (async () => {
+            const user = await getUser(id);
+            const students = await getStudents(id);
+
+            setUser(user[0]);
+            setStudents(students);
+        })()
+    }, [id])
+
     return (
         <div>
+            {user && <>
+                <div>
+                    <p>Name - {user.name}</p>
+                    <p>Email - {user.email}</p>
+                    <p>Position - {user.isAdmin ? "Admin" : "Counselor"}</p>
+                    <p>Registered students - {students.length}</p>
+                </div>
+            </>}
+
+            <Students students={students} />
             <div>
-                <p>Name - Heera Singh</p>
-                <p>Email - admin@gmail.com</p>
-                <p>Position - Admin</p>
-                <p>Registered students - 10</p>
+                <button className="shadow-sm bg-gray-800 rounded-sm text-white font-semibold px-2 py-1"
+                    onClick={() => {
+                        window.print();
+                    }}
+                >
+                    Export
+                </button>
             </div>
         </div>
     )
