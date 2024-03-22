b0836ac32aa439c27ea6c10ffe952bc78dcb604f
heera9331 <heera9331@gmail.com>
2024-03-22 06:08:00 +0530

    store created

--- counselpro-connect/src/app/layout.tsx
+++ counselpro-connect/src/app/layout.tsx
@@ -6,9 +6,12 @@ import Breadcrumbs from "@/components/Breadcrumb";
 import { usePathname } from "next/navigation";
 import { SessionProvider } from "next-auth/react";
 
+import { store } from '@/store'
+import { Provider } from 'react-redux'
+
 export default function RootLayout({ children, params }) {
   const path = usePathname();
-  console.log(params);
+  
   return (
     <html lang="en">
       <head>
@@ -16,13 +19,14 @@ export default function RootLayout({ children, params }) {
       </head>
       <body className="m-auto">
         <SessionProvider>
-          <Header />
-          <div className="scroll-smooth max-w-[1440px] mx-auto min-h-[80vh] border border-black px-2">
-            <Breadcrumbs path={path} />
-            {children}
-
-          </div>
-          <Footer />
+          <Provider store={store}>
+            <Header />
+            <div className="scroll-smooth max-w-[1440px] mx-auto min-h-[80vh] border border-black px-2">
+              <Breadcrumbs path={path} />
+              {children}
+            </div>
+            <Footer />
+          </Provider>
         </SessionProvider>
       </body>
     </html>
