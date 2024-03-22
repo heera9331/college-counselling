19b9c62e7b5596ba6fe4301ba809e2c298dc9160
heera9331 <heera9331@gmail.com>
2024-03-22 00:59:17 -0700

    linux commit

--- counselpro-connect/src/components/Header/Header.tsx
+++ counselpro-connect/src/components/Header/Header.tsx
@@ -60,7 +60,7 @@ export default function Header() {
                               item.current
                                 ? "bg-gray-900 text-white"
                                 : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                              "rounded-md px-3 py-2 text-sm font-medium"
+                              "rounded-md px-3 py-2 text-sm font-medium active:bg-gray-900"
                             )}
                             aria-current={item.current ? "page" : undefined}
                           >
