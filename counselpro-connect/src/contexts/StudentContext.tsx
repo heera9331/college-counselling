1fa8925594c5806a96292d7d83c2d032f8f7d52c
heera9331 <heera9331@gmail.com>
2024-03-27 00:17:15 +0530

    reset fnx

--- counselpro-connect/src/contexts/StudentContext.tsx
+++ counselpro-connect/src/contexts/StudentContext.tsx
@@ -8,10 +8,15 @@ const StudentContext = createContext({});
 const StudentContextProvider = ({ children }: { children: any }) => {
 
     const [state, setState] = useState<{ students: any[], status: string, error: string }>({
+        // status => initial, loading, success 
         students: [],
-        status: "idle",
+        status: "initial",
         error: ""
     })
+
+    const resetState = () => {
+        setState({ students: [], status: "initial", error: "" })
+    }
 
     const setStudents = (students: any[]) => {
         setState({ ...state, students: students });
@@ -19,16 +24,14 @@ const StudentContextProvider = ({ children }: { children: any }) => {
 
     const setError = (error: string) => {
         setState({ ...state, error: error })
-    } 
+    }
 
     const setStatus = (status: string) => {
         setState({ ...state, status: status })
     }
 
-
-
     return (
-        <StudentContext.Provider value={{ students: state.students, status: state.status, error: state.error, setStudents, setStatus, setError }}>
+        <StudentContext.Provider value={{ students: state.students, status: state.status, error: state.error, setStudents, setStatus, setError, resetState }}>
             {children}
         </StudentContext.Provider>
     )
