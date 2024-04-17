7b3b6b43ed370113a768799b009b52cb61c050ba
heera9331 <heera9331@gmail.com>
2024-04-17 23:01:46 +0530

    card controls

--- counselpro-connect/src/components/AwsCard.tsx
+++ counselpro-connect/src/components/AwsCard.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { VscTriangleLeft } from "react-icons/vsc";
+import { VscTriangleDown } from "react-icons/vsc";
+
 const AwsCard = ({
   title,
   children,
@@ -9,16 +13,30 @@ const AwsCard = ({
   titleProps?: string;
   cardProps?: string;
 }) => {
+  const [click, setClick] = useState(true);
   return (
-    <div
-      className={`m-3 bg-white-100 shadow-sm shadow-gray-600 ${cardProps}`}
-    >
+    <div className={`m-3 bg-white-100 shadow-sm shadow-gray-600 ${cardProps}`}>
       {title && (
-        <div className="bg-white-200 border-b p-3">
+        <div className="bg-white-200 border-b p-3 flex justify-between items-center">
           <h2 className={`text-2xl font-semibold ${titleProps}`}>{title}</h2>
+          {click && (
+            <VscTriangleLeft
+              onClick={() => {
+                setClick(false);
+              }}
+            />
+          )}
+          {!click && (
+            <VscTriangleDown
+              onClick={() => {
+                setClick(true);
+              }}
+            />
+          )}
+          {/* <VscTriangleRight /> */}
         </div>
       )}
-      {children && <div className="px-3 pb-3">{children}</div>}
+      {children && click && <div className="px-3 pb-3">{children}</div>}
     </div>
   );
 };
