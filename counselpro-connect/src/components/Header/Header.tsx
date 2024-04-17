5e830e0966e4762fac4b6340387ab8b45ba0f8fa
heera9331 <heera9331@gmail.com>
2024-04-17 23:48:46 +0530

    design issues

--- counselpro-connect/src/components/Header/Header.tsx
+++ counselpro-connect/src/components/Header/Header.tsx
@@ -118,14 +118,14 @@ export default function Header() {
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             <>
                               <Menu.Item>
-                                <Link href={`/profile?email=${data?.user?.email}`} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-700 hover:text-white">Your Profile</Link>
+                                <Link href={`/profile?email=${data?.user?.email}`} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-700 hover:text-gray-100">Your Profile</Link>
                               </Menu.Item>
                               <Menu.Item>
-                                <Link href={`/backup`} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-700 hover:text-white">Backup</Link>
+                                <Link href={`/backup`} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-700 hover:text-gray-100">Import/Export</Link>
                               </Menu.Item>
                               <Menu.Item>
                                 <Link href="#" >
-                                  <button onClick={() => signOut()} className="px-4 py-2 text-sm text-gray-700 w-full hover:bg-gray-700 hover:text-white">
+                                  <button onClick={() => signOut()} className="px-4 py-2 text-sm text-gray-700 w-full hover:bg-gray-700 hover:text-gray-100">
                                     <div className="flex gap-2 items-center">
                                       <IoIosLogOut className="text-xl" /><span>Logout</span>
                                     </div>
