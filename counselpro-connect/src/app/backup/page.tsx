5685fbd60d466e664a7c88512e5cea8ea63d17d5
heera9331 <heera9331@gmail.com>
2024-04-17 23:54:38 +0530

    card added

--- counselpro-connect/src/app/backup/page.tsx
+++ counselpro-connect/src/app/backup/page.tsx
@@ -1,70 +1,75 @@
-"use client"
-import { JsonToCsvExporter } from "@/components";
-import { apiBaseUrl } from "@/utils"
+"use client";
+import { JsonToCsvExporter, AwsCard } from "@/components";
+import { apiBaseUrl } from "@/utils";
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const getStudents = async () => {
-    let res = await axios.get(`${apiBaseUrl}/api/students?pageSize=1000`);
-    if (res.statusText == "OK") {
-        let data = await res.data;
-        return data;
-    }
-    return [];
-}
+  let res = await axios.get(`${apiBaseUrl}/api/students?pageSize=1000`);
+  if (res.statusText == "OK") {
+    let data = await res.data;
+    return data;
+  }
+  return [];
+};
 
 const getUsers = async () => {
-    let res = await axios.get(`${apiBaseUrl}/api/users`);
-    if (res.statusText == "OK") {
-        let data = await res.data;
-        return data;
-    }
-    return [];
-}
+  let res = await axios.get(`${apiBaseUrl}/api/users`);
+  if (res.statusText == "OK") {
+    let data = await res.data;
+    return data;
+  }
+  return [];
+};
 
 const Page = (props: any) => {
-    const [students, setStudents] = useState([]);
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(false);
+  const [students, setStudents] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        ; (async () => {
-            setLoading(true);
-            let students = await getStudents();
-            setLoading(false);
-            setStudents(students);
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      let students = await getStudents();
+      setLoading(false);
+      setStudents(students);
 
-            setLoading(true);
-            let users = await getUsers();
-            setUsers(users);
-            setLoading(false);
-        })()
-    }, [])
+      setLoading(true);
+      let users = await getUsers();
+      setUsers(users);
+      setLoading(false);
+    })();
+  }, []);
 
-    return (
-        <div>
-            <h1 className="text-2xl font-semibold">Backup</h1>
-            <div className="flex flex-col gap-2 py-2">
-                {loading ? "loading..." : <>
-                    <p className="">
-                        <span>Take backup of all the students data</span>
-                        <span className="text-blue-800 underline px-1"
-                        >
-                            <JsonToCsvExporter jsonData={students} />
-                        </span>
-                    </p>
-                    <p className="">
-                        <span>Take backup of all the counselors data</span>
-                        <span className="text-blue-800 underline px-1">
-                            <JsonToCsvExporter jsonData={users} />
-                        </span>
-                    </p>
-                </>
-                }
-
-            </div>
+  return (
+    <div className="flex">
+      <AwsCard title="Import Data" cardProps="w-[100%]">
+        <p className="bg-red-300">Note - Make sure, your data is formatted correct as neede</p>
+      </AwsCard>
+      <AwsCard title="Backup Data" cardProps="max-w-[400px]">
+        <div className="flex flex-col gap-2">
+          {loading ? (
+            "loading..."
+          ) : (
+            <>
+              <p className="">
+                <span>Take backup of all the students data</span>
+                <span className="text-blue-800 underline px-1">
+                  <JsonToCsvExporter jsonData={students} />
+                </span>
+              </p>
+              <p className="">
+                <span>Take backup of all the counselors data</span>
+                <span className="text-blue-800 underline px-1">
+                  <JsonToCsvExporter jsonData={users} />
+                </span>
+              </p>
+            </>
+          )}
         </div>
-    )
-}
+      </AwsCard>
+    </div>
+  );
+};
 
 export default Page;
