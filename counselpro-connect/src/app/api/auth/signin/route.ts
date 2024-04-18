ad9278ce69ee79aab8d51875870e7fac5dd4364c
heera9331 <heera9331@gmail.com>
2024-04-18 23:43:28 +0530

    signin route

---
+++ counselpro-connect/src/app/api/auth/signin/route.ts
@@ -0,0 +1,38 @@
+import {NextRequest,NextResponse} from 'next/server';
+import {connectDB} from "@/utils";
+import { User }from "@/models";
+import jwt from "jsonwebtoken";
+
+
+export const POST = async (req: NextRequest) => {
+    await connectDB();
+
+    let {email, password} = await req.json();
+    console.log('email', email)
+    console.log('password', password)
+
+    if(!email) {
+        return NextResponse.json({msg:"user not found"}, {status: 400});
+    }
+
+    let user = User.findOne({email, password}).select("-password");
+
+    if(!user) {
+        return NextResponse.json({msg: "invalid credentials"}, {status:404})
+    }
+    let secret = "heera";
+    let data = {...user};
+    // delete data._id;
+    let token = jwt.sign(data, secret, {expiresIn: '1h'});
+    const response =  NextResponse.json({msg:"Sign in Sucessfully",success:true,token})
+        
+    response.cookies.set("token",token,{
+        httpOnly:true
+    });
+
+    return response
+}
+
+
+
+
