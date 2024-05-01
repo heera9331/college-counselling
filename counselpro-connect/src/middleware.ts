8fc92bc08dff63eb8d0fdb8e7b2370d09380bab3
heera9331 <heera9331@gmail.com>
2024-05-01 23:41:45 +0530

    middle ware setup

--- counselpro-connect/src/middleware.ts
+++ counselpro-connect/src/middleware.ts
@@ -1,11 +1,42 @@
-import { NextResponse, NextRequest } from 'next/server'
-
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-
-    // return NextResponse.redirect(new URL('/home', request.url))
-}
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
+// import { cookies } from "next/headers";
 
 export const config = {
-    matcher: '/about/',
+  matcher: [
+    "/dashboard/:path*",
+    "/profile",
+    "/backup",
+    "/counsellor/:path*",
+    "/home/:path*",
+    "/profile/:path*",
+    "/student/:path*",
+    "/view-report/:path*",
+    "/",
+  ],
+};
+
+// protected
+
+export async function middleware(request: NextRequest) {
+  // const token = await getToken({ req: request }) || request.cookies.get("token")?.value || "";
+  console.log("middleware works");
+  const url = request.nextUrl.pathname;
+  const token = request.cookies.get("token")?.value;
+
+  console.log("token", token);
+
+  //   protected
+  if (
+    !token &&
+    (url.startsWith("/dashboard") ||
+      url.startsWith("/student") ||
+      url.startsWith("/counsellor") ||
+      url.startsWith("/view-report") ||
+      url.startsWith("/profile"))
+  )
+    return NextResponse.redirect(new URL("/login", request.url));
+
+  return NextResponse.next();
 }
