4ab4271fb045f284807f1b4956fbac4bbf057bf4
heera9331 <heera9331@gmail.com>
2024-05-01 23:44:03 +0530

    added

---
+++ counselpro-connect/src/app/api/auth/signout/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectDB } from "@/utils";
+import { User } from "@/models";
+import jwt from "jsonwebtoken";
+
+export const GET = async (req: NextRequest) => {
+   
+};
